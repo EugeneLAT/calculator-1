69cddac69503d65a334550963dd91707dc3f1b96
Dimitrijs Fedotovs <dima@fedoto.ws>
2018-02-19 23:14:39 +0200

    universal calculation (right sequence, unlimited length, mixed priority)

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.ArrayList;
+
 /**
  * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
  * @version 1.0
@@ -8,25 +10,49 @@ package calculator;
 public class Calculator {
 
     public String calculate(String[] expression) {
-        String a = expression[0];
-        String b = expression[2];
-        String op = expression[1];
+        ArrayList<String> stage = new ArrayList<>();
 
-        double da = Double.parseDouble(a);
-        double db = Double.parseDouble(b);
-
-        double result;
-        switch (op) {
-            case "+":
-                result = da + db;
-                break;
-            case "-":
-                result = da - db;
-                break;
-            default:
-                return "Error";
+        String tmp = expression[0];
+        for (int i = 1; i < expression.length; i = i + 2) {
+            String op = expression[i];
+            String next = expression[i + 1];
+            double a = Double.parseDouble(tmp);
+            double b = Double.parseDouble(next);
+            switch (op) {
+                case "+":
+                case "-":
+                    stage.add(tmp);
+                    stage.add(op);
+                    tmp = next;
+                    break;
+                case "*":
+                    tmp = String.valueOf(a * b);
+                    break;
+                case "/":
+                    tmp = String.valueOf(a / b);
+                    break;
+                default:
+                    return "Error";
+            }
         }
 
+        stage.add(tmp);
+
+        double result = Double.parseDouble(stage.get(0));
+        for (int i = 1; i < stage.size(); i += 2) {
+            String op = stage.get(i);
+            double next = Double.parseDouble(stage.get(i + 1));
+            switch (op) {
+                case "+":
+                    result = result + next;
+                    break;
+                case "-":
+                    result = result - next;
+                    break;
+                default:
+                    return "Error";
+            }
+        }
 
         return String.valueOf(result);
     }
